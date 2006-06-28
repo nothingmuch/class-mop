0f30f194be8fe9abbeb35c836d0ee0f991759a03
Yuval Kogman <nothingmuch@woobling.org>
2006-06-28 01:33:43 +0000

    Add bench dir and Bench::Point

---
+++ bench/lib/Bench/Point.pm
@@ -0,0 +1,45 @@
+#!/usr/bin/perl
+
+package Bench::Point;
+
+use strict;
+use warnings;
+
+sub new {
+    my ( $class, %params ) = @_;
+    
+    return bless {
+        x => $params{x},
+        y => $params{y},
+    }, $class;
+}
+
+sub x {
+    my ( $self, @args ) = @_;
+
+    if ( @args ) {
+        $self->{x} = $args[0];
+    }
+
+    return $self->{x};
+}
+
+sub y {
+    my ( $self, @args ) = @_;
+
+    if ( @args ) {
+        $self->{y} = $args[0];
+    }
+
+    return $self->{y};
+}
+
+sub clear {
+    my $self = shift;
+    @{ $self-> }{qw/x y/} = (0, 0);
+}
+
+__PACKAGE__;
+
+__END__
+
