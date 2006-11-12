c1c2c3ed4ec6d2d8fdb4dff0394756df6f378029
Stevan Little <stevan.little@iinteractive.com>
2006-11-12 15:52:11 +0000

    whoops

--- lib/Class/MOP.pm
+++ lib/Class/MOP.pm
@@ -11,7 +11,7 @@ use Class::MOP::Class;
 use Class::MOP::Attribute;
 use Class::MOP::Method;
 
-use Class::MOP::Class::Immutable;
+use Class::MOP::Immutable;
 
 our $VERSION   = '0.36';
 our $AUTHORITY = 'cpan:STEVAN';
