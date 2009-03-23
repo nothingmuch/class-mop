1550e0820d345fd483382e2b912ba683da3bdc1d
Dave Rolsky <autarch@houseabsolute.urth.org>
2009-03-23 10:36:09 -0500

    bump version so Moose has something to depend on

--- lib/Class/MOP/Immutable.pm
+++ lib/Class/MOP/Immutable.pm
@@ -9,7 +9,7 @@ use Class::MOP::Method::Constructor;
 use Carp         'confess';
 use Scalar::Util 'blessed';
 
-our $VERSION   = '0.78_01';
+our $VERSION   = '0.78_02';
 $VERSION = eval $VERSION;
 our $AUTHORITY = 'cpan:STEVAN';
 
