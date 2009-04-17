1d9507b6b03014d2c6f9dab49cb27a7728bde094
Shawn M Moore <sartak@gmail.com>
2009-04-16 23:41:20 -0400

    Don't define new() in this benchmark, make_immutable creates one

--- bench/lib/MOP/Immutable/Point.pm
+++ bench/lib/MOP/Immutable/Point.pm
@@ -8,11 +8,6 @@ use metaclass;
 __PACKAGE__->meta->add_attribute('x' => (accessor => 'x', default => 10));
 __PACKAGE__->meta->add_attribute('y' => (accessor => 'y'));
 
-sub new {
-    my $class = shift;
-    $class->meta->new_object(@_);
-}
-
 sub clear {
     my $self = shift;
     $self->x(0);
