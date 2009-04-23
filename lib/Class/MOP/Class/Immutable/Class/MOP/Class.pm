4325d1a7c424db9f846c6b088369a6e3470acd67
Dave Rolsky <autarch@urth.org>
2009-04-23 14:25:04 -0500

    Docs for Class::MOP::Class::Immutable::Trait Class::MOP::Class

--- lib/Class/MOP/Class/Immutable/Class/MOP/Class.pm
+++ lib/Class/MOP/Class/Immutable/Class/MOP/Class.pm
@@ -6,3 +6,32 @@ use warnings;
 use base qw(Class::MOP::Class::Immutable::Trait Class::MOP::Class);
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Class::MOP::Class::Immutable::Class::MOP::Class - An immutable subclass of Class::MOP::Class
+
+=head1 DESCRIPTION
+
+This is an empty class which inherits from
+L<Class::MOP::Class::Immutable::Trait> and L<Class::MOP::Class>.
+
+=head1 AUTHOR
+
+Yuval Kogman E<lt>nothingmuch@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2009 by Infinity Interactive, Inc.
+
+L<http://www.iinteractive.com>
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
