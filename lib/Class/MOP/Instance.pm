5fb4edd5750aadf2d20fdf45e95149272d48343a
Cory Watson <gphat@CMU-340068.WV.CC.CMU.EDU>
2009-06-25 14:30:34 -0500

    Make inline_slot_access use double quotes to be consistent (found by me, blessed by Sartak)

--- lib/Class/MOP/Instance.pm
+++ lib/Class/MOP/Instance.pm
@@ -177,7 +177,7 @@ sub inline_create_instance {
 
 sub inline_slot_access {
     my ($self, $instance, $slot_name) = @_;
-    sprintf q[%s->{"%s"}], $instance, quotemeta($slot_name);
+    sprintf q[%s->{'%s'}], $instance, quotemeta($slot_name);
 }
 
 sub inline_get_slot_value {
