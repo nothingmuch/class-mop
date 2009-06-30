9d61c6b1cf517ec84a82b1a57f05da8ac332c655
Dave Rolsky <autarch@urth.org>
2009-06-29 23:09:06 -0500

    Avoid a warning when the inherited method has no uninlined body (which is weird but seems to happen in some bizarro cases).

--- lib/Class/MOP/Method/Inlined.pm
+++ lib/Class/MOP/Method/Inlined.pm
@@ -77,7 +77,8 @@ sub can_be_inlined {
     # otherwise we have to check that the actual method is an inlined
     # version of what we're expecting
     if ( $inherited_method->isa(__PACKAGE__) ) {
-        if ( refaddr( $inherited_method->_uninlined_body )
+        if ( $inherited_method->_uninlined_body
+             && refaddr( $inherited_method->_uninlined_body )
              == refaddr($expected_method) ) {
             return 1;
         }
