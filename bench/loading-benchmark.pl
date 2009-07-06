511430f99bbe045bf632e0b059a78cf4f271e18f
gfx <gfuji@cpan.org>
2009-07-06 16:20:16 +0900

    add bench/oading-benchmark.pl

---
+++ bench/loading-benchmark.pl
@@ -0,0 +1,14 @@
+#!perl -w
+use strict;
+use Benchmark qw(:all);
+
+my $module = 'Moose';
+
+cmpthese timethese 10 => {
+	released => sub{
+		system($^X, '-e', "require $module") == 0 or die;
+	},
+	blead => sub{
+		system($^X, '-Iblib/lib', '-Iblib/arch', '-e', "require $module") == 0 or die;
+	},
+};
