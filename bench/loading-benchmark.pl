7b4f6e93f6c68ee06ca2adfb380afdcac556efef
Dave Rolsky <autarch@urth.org>
2009-07-06 09:15:18 -0500

    no Win32 line endings, code formatting tweaks

--- bench/loading-benchmark.pl
+++ bench/loading-benchmark.pl
@@ -5,10 +5,12 @@ use Benchmark qw(:all);
 my $module = 'Moose';
 
 cmpthese timethese 10 => {
-	released => sub{
-		system($^X, '-e', "require $module") == 0 or die;
-	},
-	blead => sub{
-		system($^X, '-Iblib/lib', '-Iblib/arch', '-e', "require $module") == 0 or die;
-	},
+    released => sub {
+        system( $^X, '-e', "require $module" ) == 0 or die;
+    },
+    blead => sub {
+        system( $^X, '-Iblib/lib', '-Iblib/arch', '-e', "require $module" )
+            == 0
+            or die;
+    },
 };
