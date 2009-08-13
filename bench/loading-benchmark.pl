41070b4eebf43492380a31b9fbab69b329cf8616
gfx <g.psy.va@gmail.com>
2009-08-13 14:26:38 +0900

    Enhance bench/loading-benchmark.pl to try to load blead Moose

--- bench/loading-benchmark.pl
+++ bench/loading-benchmark.pl
@@ -6,13 +6,21 @@ my($count, $module) = @ARGV;
 $count  ||= 10;
 $module ||= 'Moose';
 
+my @blib      = qw(-Iblib/lib -Iblib/arch -I../Moose/blib/lib -I../Moose/blib/arch -I../Moose/lib);
+
+$| = 1; # autoflush
+
+print 'Installed: ';
+system $^X,        '-le', 'require Moose; print $INC{q{Moose.pm}}';
+
+print 'Blead:     ';
+system $^X, @blib, '-le', 'require Moose; print $INC{q{Moose.pm}}';
+
 cmpthese timethese $count => {
     released => sub {
-        system( $^X, '-e', "require $module" ) == 0 or die;
+        system( $^X,        '-e', "require $module") == 0 or die;
     },
     blead => sub {
-        system( $^X, '-Iblib/lib', '-Iblib/arch', '-e', "require $module" )
-            == 0
-            or die;
+        system( $^X, @blib, '-e', "require $module") == 0 or die;
     },
 };
