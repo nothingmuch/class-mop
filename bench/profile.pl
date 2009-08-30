c6bdbe978bf0109afdb6f1d81cfb8baab85a8cdd
gfx <gfuji@cpan.org>
2009-08-30 14:48:27 +0900

    Improve bench/profile.pl

--- bench/profile.pl
+++ bench/profile.pl
@@ -1,9 +1,6 @@
 #!perl -w
 # Usage: perl bench/profile.pl (no other options including -Mblib are reqired)
-
 use strict;
-
-my $script = 'bench/foo.pl';
 
 my $branch = do{
 	open my $in, '.git/HEAD' or die "Cannot open .git/HEAD: $!";
@@ -14,10 +11,14 @@ my $branch = do{
 	$s;
 };
 
-print "Profiling $branch ...\n";
+my $extra = shift || 'KiokuDB';
 
-my @cmd = ($^X, '-Iblib/lib', '-Iblib/arch', $script);
+print "Profiling $branch (require $extra) ...\n";
+
+my @cmd = ($^X, '-Iblib/lib', '-Iblib/arch', '-e', "require Moose; require $extra");
 print "> @cmd\n";
+system(@cmd) == 0 or die "Cannot profile";
+system(@cmd) == 0 or die "Cannot profile";
 system(@cmd) == 0 or die "Cannot profile";
 
 @cmd = ($^X, '-S', 'nytprofhtml', '--out', "nytprof-$branch");
